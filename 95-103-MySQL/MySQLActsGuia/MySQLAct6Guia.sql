52dd4a1aaafa5040ed7c89938539d96c8cf025fd
Joaquin-CasVar <joaquincastellanovarela14@gmail.com>
2023-09-27 19:20:40 -0300

    Actividades de la guia de MySQL

---
+++ 95-103-MySQL/MySQLActsGuia/MySQLAct6Guia.sql
@@ -0,0 +1,8 @@
+-- Act 6
+
+-- a)
+SELECT * FROM personajes;
+-- b)
+SELECT nombre_real FROM personajes;
+-- c)
+SELECT nombre_real FROM personajes WHERE nombre_real LIKE 'b%';
---
+++ 95-103-MySQL/MySQLActsGuia/MySQLAct7Guia.sql
@@ -0,0 +1,2 @@
+-- Act 7
+SELECT * FROM personajes ORDER BY inteligencia;
